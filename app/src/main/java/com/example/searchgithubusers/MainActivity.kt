06f1f317a878b2304dbf2eed64684591321ab158
yaerse <yaerse@yahoo.com.tw>
2022-02-12 16:26:25 +0800

    [新增] 增加ViewBinding的功能

--- app/src/main/java/com/example/searchgithubusers/MainActivity.kt
+++ app/src/main/java/com/example/searchgithubusers/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.searchgithubusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.searchgithubusers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        Toast.makeText(this, "${binding.mainHello.text}", Toast.LENGTH_SHORT).show();
     }
 }
